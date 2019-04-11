a2ad8715fbe294ca7b376ef3ba8131c5c0e579bc
Brian Tobal <brian.tobal@gmail.com>
2019-04-11 19:42:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   
   def add_item(title, price, quantity=1)
     @total += (price * quantity)
-    @self
+    @self.last_trans = 
     quantity.times do 
      @items << title
     end 
