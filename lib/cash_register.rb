a686d691f96d1eabac02a55334943fcbe17abba0
Brian Tobal <brian.tobal@gmail.com>
2019-04-11 19:29:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,8 +10,8 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity=1)
-    @items << title
     @total += (price * quantity)
+    @items << title
   end 
   
   def apply_discount
